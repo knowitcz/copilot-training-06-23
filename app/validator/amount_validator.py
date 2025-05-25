7db976a97a78a7aa6aaf36773a1cf7db129d7f49
Jiří Weiser <jiri.weiser@knowit.cz>
2025-05-26 00:37:44 +0200

    Task-5: Validate online transaction amount to be greater than 0

--- app/validator/amount_validator.py
+++ app/validator/amount_validator.py
@@ -1,4 +1,15 @@
-class AmountValidator:
-    def validate(self, amount: int) -> None:
+from typing import Protocol
+
+class AmountValidatorProtocol(Protocol):
+    def __call__(self, amount: int) -> None:
+        ...
+
+class AmountValidator(AmountValidatorProtocol):
+    def __call__(self, amount: int) -> None:
         if amount > 10000:
             raise ValueError("Amount cannot exceed 10000.")
+
+class PositiveAmountValidator(AmountValidatorProtocol):
+    def __call__(self, amount: int) -> None:
+        if amount <= 0:
+            raise ValueError("Amount must be greater than 0.")
