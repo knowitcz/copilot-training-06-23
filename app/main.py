2e49fb7f1cfaae51d0d83b597d3df4075b90290c
Jiří Weiser <jiri.weiser@knowit.cz>
2025-05-26 00:17:05 +0200

    Task-3: Implement online banking features with transfer functionality

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.db import SQLModel, engine
 from app.api.account_routes import router as account_router
+from app.api.bank_routes import router as bank_router
 from app.startup import create_default_accounts
 
 SQLModel.metadata.create_all(bind=engine)
@@ -14,5 +15,6 @@ def serve_static_files():
     return StaticFiles(directory="app/static", html=True)
 
 app.include_router(account_router, prefix="/api/v1", tags=["account"])
+app.include_router(bank_router, prefix="/api/v1", tags=["bank"])
 
 create_default_accounts()
